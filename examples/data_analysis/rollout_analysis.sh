5154cd77d2d6d48df263b014f3075f432fc99626
Yibiao Ying <yibiaoy@amazon.com>
2025-07-24 01:30:06 +0000

    add bash file for offline rollout analysis

---
+++ examples/data_analysis/rollout_analysis.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+source "/code/yibiaoy-sandbox/miniconda3/etc/profile.d/conda.sh"
+conda activate verl
+
+export HF_HOME="/checkpoints/yibiaoy-sandbox/HF"
+export HF_HUB_OFFLINE=1
+
+
+dir1='/data/yibiaoy-sandbox/skywork-or1/qwen3-32b_generation_shards/'
+dir2='/data/yibiaoy-sandbox/skywork-or1/qwen3-32b_generation_part1_shards//'
+dirs=( "$dir1" "$dir2" )
+files_array=()
+for d in "${dirs[@]}"; do
+  files_array+=( "$d"* )
+done
+formatted_files=$(printf ",'%s'" "${files_array[@]}")
+formatted_files="[${formatted_files#?}]"
+offline_rollout_files_list="$formatted_files"
+echo "Rollout Files: $offline_rollout_files_list"
+
+# offline_rollout_files_list="/data/yibiaoy-sandbox/skywork-or1/qwen3-32b_generation_part1_shards/shard_00001.parquet"
+# offline_rollout_files_list="/data/yibiaoy-sandbox/skywork-or1/qwen3-32b_generation_shards/shard_00001.parquet"
+
+python3 -m verl.trainer.main_rollout_data_analysis \
+    data.path=$offline_rollout_files_list \
+    data.prompt_key=prompt \
+    data.response_key=responses \
+    data.data_source_key=data_source \
+    data.output_path=/data/yibiaoy-sandbox/skywork-or1/qwen3-32b_generation_detailed_only_hard.parquet \
+    data.reward_model_key=reward_model  2>&1 | tee /code/yibiaoy-sandbox/verl/logs/offline_generation/qwen3-32b_generation.log
+
+
+# dir1="/data/yibiaoy-sandbox/skywork-or1/distill_qwen_1p5b_generation_shards/"
+# dirs=("$dir1")
+# files_array=()
+# for d in "${dirs[@]}"; do
+#   files_array+=( "$d"* )
+# done
+# formatted_files=$(printf ",'%s'" "${files_array[@]}")
+# formatted_files="[${formatted_files#?}]"
+# offline_rollout_files_list="$formatted_files"
+# echo "Rollout Files: $offline_rollout_files_list"
+
+# python3 -m verl.trainer.main_rollout_data_analysis \
+#     data.path=$offline_rollout_files_list \
+#     data.prompt_key=prompt \
+#     data.response_key=responses \
+#     data.data_source_key=data_source \
+#     data.output_path=/data/yibiaoy-sandbox/skywork-or1/distill_qwen_1p5b_generation_detailed.parquet \
+#     data.reward_model_key=reward_model  2>&1 | tee /code/yibiaoy-sandbox/verl/logs/offline_generation/distill_qwen_1p5b_generation.log
+
+
+# dir1="/data/yibiaoy-sandbox/skywork-or1/distill_qwen_1p5b_800steps_generation_shards/"
+# dirs=("$dir1")
+# files_array=()
+# for d in "${dirs[@]}"; do
+#   files_array+=( "$d"* )
+# done
+# formatted_files=$(printf ",'%s'" "${files_array[@]}")
+# formatted_files="[${formatted_files#?}]"
+# offline_rollout_files_list="$formatted_files"
+# echo "Rollout Files: $offline_rollout_files_list"
+
+# python3 -m verl.trainer.main_rollout_data_analysis \
+#     data.path=$offline_rollout_files_list \
+#     data.prompt_key=prompt \
+#     data.response_key=responses \
+#     data.data_source_key=data_source \
+#     data.output_path=/data/yibiaoy-sandbox/skywork-or1/distill_qwen_1p5b_800steps_generation_detailed.parquet \
+#     data.reward_model_key=reward_model  2>&1 | tee /code/yibiaoy-sandbox/verl/logs/offline_generation/distill_qwen_1p5b_800steps_generation.log
